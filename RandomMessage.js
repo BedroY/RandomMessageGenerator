8bce2fa3f840624bb679f3c410adc40eb455b6ea
BedroY <bedran_1999@hotmail.com>
2022-01-05 14:04:23 +0100

    fixed calling of the function on line 16

--- RandomMessage.js
+++ RandomMessage.js
@@ -1,7 +1,7 @@
-const random = Math.floor(Math.random() * 3);
+let random = Math.floor(Math.random() * 3);
 
 const Messages = ["The dreams of those who've fallen! The hopes of those who'll follow! Those two sets of dreams weave together into a double helix! Drilling a path towards tomorrow! And that's Tengen Toppa! That's Gurren Lagann! - Simon",
- "God gave us eyes at the front of our heads so we can look forward to the future. - Kamina", "If there's a wall in our way then we smash it down! If there isn't a path, then we carve one ourselves!";
+ "God gave us eyes at the front of our heads so we can look forward to the future. - Kamina", "If there's a wall in our way then we smash it down! If there isn't a path, then we carve one ourselves!"]
 
  function randomMessage(random, Messages) {
      if (random === 1){
@@ -13,4 +13,4 @@ const Messages = ["The dreams of those who've fallen! The hopes of those who'll 
      }
  }
 
- randomMessages(random,Messages);
+ randomMessage(random,Messages);
